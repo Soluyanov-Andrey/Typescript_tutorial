7408c0ef3279181e398ed17f2e3b46264867ffcc
АндрейСолуянов <soluyanov.a@yandex.ru>
2021-06-01 09:18:46 +0300

    Пример 2-2

---
+++ 2/2.js
@@ -0,0 +1,6 @@
+function r(_a) {
+    var a = _a[0], b = _a[1];
+    console.log(a);
+    console.log(b);
+}
+r(["Hello", 45]);
--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 // import './1-basic-type/1';
 // import './1-basic-type/11';
-import './2/1';
+import './2/2';
