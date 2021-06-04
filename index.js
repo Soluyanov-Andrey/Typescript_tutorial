26326b89008ba9d2adc9828c7fdec62fabfe1e16
АндрейСолуянов <soluyanov.a@yandex.ru>
2021-06-04 21:27:31 +0300

    4-1

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ exports.__esModule = true;
 // import './1-basic-type/1';
 // import './1-basic-type/11';
 //import './2-examples/5';
-require("./3-examples-function/7");
+//import './3-examples-function/7';
+require("./4-examles-genetics/1");
--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 // import './1-basic-type/1';
 // import './1-basic-type/11';
 //import './2-examples/5';
-import './3-examples-function/7';
+//import './3-examples-function/7';
+import './4-examles-genetics/1';
