5dc353be74282c374a7cfc4142bb18dc656602d6
АндрейСолуянов <soluyanov.a@yandex.ru>
2021-06-05 22:37:08 +0300

    refact

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 "use strict";
 exports.__esModule = true;
 // import './1-basic-type/1';
-// import './1-basic-type/11';
-//import './2-examples/5';
-//import './3-examples-function/7';
+// import './2-examples/5';
+// import './3-examples-function/7';
 require("./4-examles-generics/7");
--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 // import './1-basic-type/1';
-// import './1-basic-type/11';
-//import './2-examples/5';
-//import './3-examples-function/7';
+// import './2-examples/5';
+// import './3-examples-function/7';
 import './4-examles-generics/7';
